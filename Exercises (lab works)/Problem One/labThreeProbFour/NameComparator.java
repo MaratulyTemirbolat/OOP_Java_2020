78142a06db847125852dc5708858f4151ceb97c2
MaratulyTemirbolat <47031729+MaratulyTemirbolat@users.noreply.github.com>
2022-01-26 11:30:16 +0600

    Delete Exercises (lab works)/Problem One/labThreeProbFour directory

--- Exercises (lab works)/Problem One/labThreeProbFour/NameComparator.java
+++
@@ -1,9 +0,0 @@
-package labThreeProbFour;
-
-import java.util.Comparator;
-
-public class NameComparator implements Comparator<Employee>{
-	public int compare(Employee em1, Employee em2) {
-		return em1.getName().compareTo(em2.getName());
-	}
-}
